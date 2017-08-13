e691423db39a972957eeceabdf24fc30ae760bbd
Umang Vanjara <umang.vanjara@gmail.com>
2017-08-12 22:58:34 -0700

    adding initial movie trailer project code

---
+++ media.py
@@ -0,0 +1,18 @@
+import webbrowser
+
+# My Movie Class
+class Movie():
+	#def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+	# Defining the constructor
+	def __init__(self, movie_title, poster_image, trailer_youtube):
+			# Movie Title
+			self.title = movie_title
+			#self.storyline = movie_storyline
+			# Movie Poster
+			self.poster_image_url = poster_image
+			# Youtube trailer link
+			self.trailer_youtube_url = trailer_youtube
+
+	# Method to show the trailer
+	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url)
