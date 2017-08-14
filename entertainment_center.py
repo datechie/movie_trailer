eeb33e7c6fd6f1c43886031ce0973a90403f79ae
datechie <umang.vanjara@gmail.com>
2017-08-13 23:46:10 -0700

    updated entertainment_center.py and media.py to meet PEP8

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,23 +3,30 @@ import fresh_tomatoes # Provided script for generating the web page
 import media # My Movies class
 
 # Defining my movie objects
+# Defining a base URL to reduce the poster URL to meet PEP8 requirments
+wiki_base_url = "https://upload.wikimedia.org/wikipedia/en"
+
+# Movie 1
 wonder_woman = media.Movie(
-					"Wonder Woman", 
-					"https://upload.wikimedia.org/wikipedia/en/e/ed/Wonder_Woman_%282017_film%29.jpg",
-					"https://www.youtube.com/watch?v=1Q8fG0TtVAY")
+            "Wonder Woman",
+            wiki_base_url + "/e/ed/Wonder_Woman_%282017_film%29.jpg",
+            "https://www.youtube.com/watch?v=1Q8fG0TtVAY")
 
+# Movie 2
 the_matrix = media.Movie(
-					"The Matrix", 
-					"https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
-					"https://www.youtube.com/watch?v=m8e-FF8MsqU")
+            "The Matrix",
+            wiki_base_url + "/c/c1/The_Matrix_Poster.jpg",
+            "https://www.youtube.com/watch?v=m8e-FF8MsqU")
 
+# Movie 3
 sholay = media.Movie(
-				"Sholay",
-				"https://upload.wikimedia.org/wikipedia/en/5/52/Sholay-poster.jpg",
-				"https://www.youtube.com/watch?v=EwT6RdS-Nac")
+            "Sholay",
+            wiki_base_url + "/5/52/Sholay-poster.jpg",
+            "https://www.youtube.com/watch?v=EwT6RdS-Nac")
 
 # Creating the list of movies
 movies = [wonder_woman, the_matrix, sholay]
 
 # Generating the movie trailers web page
 fresh_tomatoes.open_movies_page(movies)
+
--- media.py
+++ media.py
@@ -1,20 +1,21 @@
-
 # Import required modules
 import webbrowser
 
+
 # My Movie Class
 class Movie():
-	''' This is the Movie class that requires 3 inputs - 
-	Movie Title, Movie Poster Image, Youtube trailer link '''
-	# Defining the constructor
-	def __init__(self, movie_title, poster_image, trailer_youtube):
-			# Movie Title
-			self.title = movie_title
-			# Movie Poster
-			self.poster_image_url = poster_image
-			# Youtube trailer link
-			self.trailer_youtube_url = trailer_youtube
+    ''' This is the Movie class that requires 3 inputs -
+    Movie Title, Movie Poster Image, Youtube trailer link '''
 
-	# Method to show the trailer
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+    # Defining the constructor
+    def __init__(self, movie_title, poster_image, trailer_youtube):
+        # Movie Title
+        self.title = movie_title
+        # Movie Poster
+        self.poster_image_url = poster_image
+        # Youtube trailer link
+        self.trailer_youtube_url = trailer_youtube
+
+    # Method to show the trailer
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
